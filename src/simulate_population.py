25cc2bfade8665cbd44d3592fcf2720a2b225eed
Taylor Kessinger <taylor.kessinger@uky.edu>
2017-03-10 17:04:55 -0500

    new and improved simulation script

--- src/simulate_population.py
+++ src/simulate_population.py
@@ -7,6 +7,7 @@ Description: Simulation of valley crossing using FFPopSim.
     Also TODO: direct calculation of Nsigma_b.
 '''
 import sys
+sys.path.append('/home/tkessinger/tools/FFPopSim/pkg/python/')
 import numpy as np
 import FFPopSim as h
 import random as rand
@@ -14,24 +15,26 @@ import matplotlib.pyplot as plt
 from time import time
 import argparse
 import math
+import os
 import cPickle as pickle
+
 
 t0 = time()
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--N', type=int, default=10000)
-parser.add_argument('--s', type=float, default=0.1)
-parser.add_argument('--delta', type=float, default=0.1)
+parser.add_argument('--s', type=float, default=0.001)
+parser.add_argument('--delta', type=float, default=0.01)
 parser.add_argument('--rho', type=float, default=0.0)
-parser.add_argument('--sigma', type=float, default=0.1)
-parser.add_argument('--mu', type=float, default=0.001)
+parser.add_argument('--sigma', type=float, default=0.01)
+parser.add_argument('--mu', type=float, default=0.0001)
 parser.add_argument('--numsamples',type=int,default=10)
 parser.add_argument('--runno', type=int, default=0)
 parser.add_argument('--name', type=str, default='test')
 
 args=parser.parse_args()
 
-L = 300 #number of loci. 1000 is about as many as we're likely to need.
+L = 100 #number of loci. 1000 is about as many as we're likely to need.
 N = args.N #population size. recall that Nsigma is the main parameter.
 mu = args.mu #per site mutation rate for focal mutations
 sigma = args.sigma #fitness variance
@@ -42,21 +45,24 @@ cx = 0 #per locus crossover rate
 s = args.s
 delta = args.delta
 
-fitness_check = N*10 #for debugging: number of generations to let pass before checking the gaussianity of the fitness distribution
-
 mutant_locs = np.array([np.int32(L*0.35), np.int32(L*0.65)]) #locations of the focal mutations: this should avoid "edge" effects
 #i am not sure why int32 is required
-deleterious_effects = [delta,delta] #fitness costs of single mutants
-beneficial_effect = s #fitness advantage of double mutant
 
-pop = h.haploid_highd(L, most_polymorphic = True) #high-dimensional simulation
+deleterious_effects = [s/4,s/4]
+beneficial_effect = delta/2+s/4
+#surprisingly, these are the coefficients needed for single and double mutants to have the desired fitnesses.
+#FFPopSim works on a {-1,1} basis, not a {0,1} basis, so
+#F = \sum_i f_i s_i + \sum_{i<j} f_{ij} s_i s_j + ...
+#the multiplication of indices is what makes the above necessary
+
+pop = h.haploid_highd(L, number_of_traits=2, most_polymorphic = True) #high-dimensional simulation
 #note: h.haploid_lowd() implements epistasis more easily, but we need highd to study draft
 #note: all_polymorphic is an infinite sites model.
 #loci whose frequencies hit 0 or 1 are automatically "injected" somewhere.
 #if they fixed, their selection coefficients are flipped.
-#i believe FFPopSim has some kind of flag to see whether fixation/extinction have occurred (need to look for this or write it).
+#most_polymorphic is just a hacked-together version that allows us to control the mutation rates of some loci.
 pop.carrying_capacity = N
-pop.mutation_rate = mu
+pop.mutation_rate = 0
 pop._set_mutant_loci(mutant_locs)
 pop.recombination_model = h.CROSSOVERS #recombination model, can be either FREE_RECOMBINATION or CROSSOVERS
 pop.outcrossing_rate = rho
@@ -64,10 +70,11 @@ pop.crossover_rate = cx
 
 pop.set_wildtype(N)
 
-s_mean = 0.01 #average effect of beneficial mutations: this turns out to be mostly irrelevant due to sigma, above
-burn_time = N #number of generations to wait for population to equilibrate: ideally this should be of order T_MRCA
+s_mean = 1e-6 #average effect of beneficial mutations: this turns out to be mostly irrelevant due to sigma, above
+base_burn_time = N #number of generations to wait for population to equilibrate: ideally this should be of order T_MRCA
+burn_time = base_burn_time
 burned_in = False #flag for whether the population has equilibrated
-run_time = 5*N #number of generations to simulate
+run_time = 1e6 #number of generations to simulate
 
 num_attempts = 0 #attempts at crossing the valley, i.e., number of deleterious or neutral single mutants
 num_beneficial = 0 #number of beneficial double mutants
@@ -76,66 +83,136 @@ mutations_in_play = [False,False] #flag to see whether the mutations are current
 double_mut_in_play = False
 weights = [[],[]] #a record of the size of mutant bubbles
 dm_weights = []
+bubble_openings = [[],[]]
+dm_bubble_openings = []
+bubble_closings = [[],[]]
+dm_bubble_closings = []
+
+dm_successful_crossings = []
+
+mut_freqs = [[],[]]
+dm_freqs = []
+
+
 
 selection_coefficients = np.random.exponential(s_mean, size = L) #initialize fitness function
+selection_coefficients[mutant_locs]=1e-10
 #NOTE: due to an FFPopSim quirk, selection coefficients of 0 are incompatible with all_polymorphic.
 #use something like 1e-10 instead.
 
-pop.set_trait_additive(selection_coefficients)
-pop.add_trait_coefficient(-0.5, [mutant_locs[0]])
-pop.add_trait_coefficient(-0.5, [mutant_locs[1]]) #set the fitness of the deleterious mutants to (effectively) zero.
-pop.add_trait_coefficient(beneficial_effect, mutant_locs) #set the epistatic effect
+pop.set_trait_additive(selection_coefficients,0)
+pop.set_trait_additive(np.zeros(L),1)
+
+pop.add_trait_coefficient(deleterious_effects[0],[mutant_locs[0]],1)
+pop.add_trait_coefficient(deleterious_effects[1],[mutant_locs[1]],1) #let the deleterious loci start drifting
+pop.add_trait_coefficient(beneficial_effect, mutant_locs,1) #set the epistatic effect
 
 afreqs = []
 
 while pop.generation < run_time:
-    if not pop.generation%fitness_check:
-        #check the histogram for gaussianity manually
-        pfhist = pop.get_fitness_histogram(100)
-        plt.figure()
-        plt.plot((pfhist[1][1:]+pfhist[1][:-1])*1.0/2,pfhist[0])
-        ax=plt.gca()
-        ax.set_yscale('log') #on a log scale, the fitness distribution should be parabolic
-        plt.show()
-    pfit = pop.get_fitness_statistics()
-    if pfit.variance > 0 and pop.generation > 100:
-        pop.trait_weights *= sigma/np.sqrt(pfit.variance) #normalize the selection coefficients so that sigma is constant.
-        #TODO: exclude the focal loci from this calculation.
+    pfit = pop.get_trait_statistics(0)
+    if pfit.variance > 0 and pop.generation > np.min([100, burn_time]):
+        multiplier = sigma/np.sqrt(pfit.variance)
+        selection_coefficients *= multiplier
+        selection_coefficients[mutant_locs]=1e-10
+        pop.set_trait_additive(selection_coefficients,0)
+        #normalizes the selection coefficients so that sigma stays constant.
+        #the above is clunky compared to pop.trait_weights *= np.array([multiplier,1]),
+        #but FFPopSim seems not to like trait weights higher than 1,
+        #and it will gladly let trait weights collapse to 0 when there is more than one trait
+    if pfit.variance == 0 and pop.generation > 100:
+        print "error: zero variance"
     if pop.generation > burn_time and burned_in == False: #if the population has equilibrated
-        pop.add_trait_coefficient(deleterious_effects[0],[mutant_locs[0]])
-        pop.add_trait_coefficient(deleterious_effects[1],[mutant_locs[1]]) #let the deleterious loci start drifting
+        pop.trait_weights += np.array([0,1.0]) #"turn on" the focal loci
+        pop.mutation_rate = mu #set the focal loci to start mutating
         burned_in = True
     if burned_in == True:
-        #this loop is currently never called due to a quirk in all_polymorphic (TODO).
-        #in principle the idea is to see if the new locus has entered the population anew.
-        #because all_polymorphic automatically seeds extinct alleles in one individual,
-        #this is impossible to determine (frequency is never allowed to hit zero).
-        #there are a few possible ways around this.
         for di, del_locus in enumerate(mutant_locs):
-            if pop.get_allele_frequency(del_locus) > 0 and mutations_in_play[di] == False:
+            if pop.get_allele_frequency(del_locus) != 0 and mutations_in_play[di] == False:
             #if either mutant has appeared
                 mutations_in_play[di] = True
+                bubble_openings[di].append(pop.generation)
                 num_attempts += 1
                 weights[di].append([1]) #start a new bubble of size 1
             elif mutations_in_play[di] == True:
                 if pop.get_allele_frequency(del_locus) == 0:
                     mutations_in_play[di] = False #if it's gone extinct, close the bubble
+                    bubble_closings[di].append(pop.generation)
                 else:
-                    weights[di][-1].append(int(pop.get_allele_frequency(del_locus)*pop.N)) #add the current size to the bubble history
-        if pop.get_pair_frequency(mutant_locs[0], mutant_locs[1]) > 0 and double_mut_in_play == False:
+                    weights[di][-1].append(int(np.ceil(pop.get_allele_frequency(del_locus)*pop.N))) #add the current size to the bubble history
+                    #ceil is needed to avoid rounding errors
+        if pop.get_pair_frequency(mutant_locs[0], mutant_locs[1]) != 0 and double_mut_in_play == False:
         #if the double mutant has appeared
             double_mut_in_play = True
+            dm_bubble_openings.append(pop.generation)
             num_beneficial += 1
-            dm_weights.append([1]) #start a double mutant bubble of size 1
+            dm_weights.append([int(np.ceil(pop.get_pair_frequency(mutant_locs[0],mutant_locs[1])*pop.N))])
+            #start a double mutant bubble of appropriate size (again, ceil avoids rounding errors)
         elif double_mut_in_play == True:
             if pop.get_pair_frequency(mutant_locs[0],mutant_locs[1]) == 0:
+                dm_bubble_closings.append(pop.generation)
                 double_mut_in_play = False
+            elif pop.get_pair_frequency(mutant_locs[0],mutant_locs[1]) > 0.9:
+                #frequency 0.9 seems like about as good a cutoff as any
+                dm_successful_crossings.append(pop.generation-burn_time) #record the amount of time elapsed since the last burn-in
+                print 'successful crossing', pop.generation
+                
+                #the below "clears out" the loci and reinitializes the population with the same appropriate allele frequencies
+                allele_freqs = pop.get_allele_frequencies()
+                allele_freqs[mutant_locs] = 0
+                pop.set_allele_frequencies(allele_freqs,pop.N)
+                pop.carrying_capacity = N #reset the carrying capacity to its desired value
+                
+                selection_coefficients = np.random.exponential(s_mean, size = L) #we might as well change up the background fitnesses at this point
+                selection_coefficients[mutant_locs]=1e-10
+                
+                double_mut_in_play = False
+                
+                pop.mutation_rate = 0
+                burn_time = pop.generation + 0.1*base_burn_time #we might as well let the population equilibrate again for a bit
+                burned_in = False
+                pop.trait_weights += np.array([0,-1.0]) #"turn off" the focal loci: just a failsafe
+
+                
             else:
-                dm_weights[-1].append(int(pop.get_pair_frequency(mutant_locs[0],mutant_locs[1])*pop.N))
-    afreqs.append(pop.get_allele_frequencies())
+                dm_weights[-1].append(int(np.ceil(pop.get_pair_frequency(mutant_locs[0],mutant_locs[1])*pop.N)))
+    dm_freqs.append(pop.get_pair_frequency(mutant_locs[0],mutant_locs[1]))
+    mut_freqs[0].append(pop.get_allele_frequency(mutant_locs[0]))
+    mut_freqs[1].append(pop.get_allele_frequency(mutant_locs[1]))
     pop.evolve(1)
 
+plt.figure()
+plt.plot(mut_freqs[0][::10])
+plt.plot(mut_freqs[1][::10])
+plt.plot(dm_freqs[::10])
+plt.show()
 
-afreqs = np.array(afreqs)
+prefix = 'N_'+str(N)+'_mu_'+str(mu)+'_s_'+str(s)+'_delta_'+str(delta)
+
+if not os.path.exists('output/valley_crossing_sims_'+args.name):
+    os.makedirs('output/valley_crossing_sims_'+args.name)
+
+with open('output/valley_crossing_sims_'+args.name+'/'+prefix+'_dm_weights_'+str(args.runno)+'.pkl', 'w') as dm_weights_file:
+    pickle.dump(dm_weights, dm_weights_file)
+with open('output/valley_crossing_sims_'+args.name+'/'+prefix+'_weights_'+str(args.runno)+'.pkl', 'w') as weights_file:
+    pickle.dump(weights, weights_file)
+with open('output/valley_crossing_sims_'+args.name+'/'+prefix+'_dm_openings_'+str(args.runno)+'.pkl', 'w') as dm_openings_file:
+    pickle.dump(bubble_openings, dm_openings_file)
+with open('output/valley_crossing_sims_'+args.name+'/'+prefix+'_openings_'+str(args.runno)+'.pkl', 'w') as openings_file:
+    pickle.dump(dm_bubble_openings, openings_file)
+with open('output/valley_crossing_sims_'+args.name+'/'+prefix+'_dm_closings_'+str(args.runno)+'.pkl', 'w') as dm_closings_file:
+    pickle.dump(dm_bubble_closings, dm_closings_file)
+with open('output/valley_crossing_sims_'+args.name+'/'+prefix+'_closings_'+str(args.runno)+'.pkl', 'w') as closings_file:
+    pickle.dump(bubble_closings, closings_file)
+
+with open('output/valley_crossing_sims_'+args.name+'/'+prefix+'_dm_successes_'+str(args.runno)+'.pkl', 'w') as dm_successes_file:
+    pickle.dump(dm_successful_crossings, dm_successes_file)
+
+
+
+#afreqs = np.array(afreqs)
 #still needed:
 #calculate Nsigma_b directly.
+
+#print len(dm_weights)
+#print len(weights[0]),len(weights[1])
